97fbd15fe71c08a7db2300e257f172890d5cff74
Anton <34812122+antonio-git@users.noreply.github.com>
2018-09-18 22:05:38 +0300

    try header

--- bitrix/templates/fashify/footer.php
+++ bitrix/templates/fashify/footer.php
@@ -1,146 +1,58 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Fashify
+ */
 
-<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
+?>
 
+	</div><!-- #content -->
 
-<footer id="colophon" class="site-footer" role="contentinfo">
+	<footer id="colophon" class="site-footer" role="contentinfo">
 
-	<div class="footer-staff-picks">
+		<?php $enable_staff_picks = get_theme_mod( 'fashify_staff_picks', '' ) ;
 
-		<div class="container">
+		if ( $enable_staff_picks == true ) {
+			get_template_part( 'template-parts/content', 'staff' );
+		}
 
-			<h3>Staff Picks</h3>
+		?>
 
-			<div class="staff-inner">
+		<?php if ( is_active_sidebar( 'footer' ) ) { ?>
+		<div class="footer-widgets">
+			<div class="container">
+				<div class="footer-inner">
+					<?php
+							dynamic_sidebar( 'footer' );
+					?>
+				</div>
+			</div>
+		</div>
+		<?php } ?>
 
-				<article id="post-50-recent" class="post-50 post type-post status-publish format-standard has-post-thumbnail hentry category-design category-mobile category-photo category-video tag-design">
+		<div class="site-info">
+			<div class="container">
 
-					<div class="featured-image">
-						<a href="https://wpstash.com/fashify/2016/07/06/samsung-vs-apple-samsung-is-winning-every-way-but-one/" title="Samsung vs. Apple: Samsung Is Winning Every Way But One"><img width="280" height="220" src="https://wpstash.com/fashify/wp-content/uploads/sites/5/2016/07/pexels-photo-smartphone-macbook-280x220.jpg" class="attachment-fashify-thumb-layout3 size-fashify-thumb-layout3 wp-post-image" alt="" /></a>				</div>
-
-						<header class="entry-header">
-
-							<h2 class="entry-title"><a href="https://wpstash.com/fashify/2016/07/06/samsung-vs-apple-samsung-is-winning-every-way-but-one/" rel="bookmark">Samsung vs. Apple: Samsung Is Winning Every Way But One</a></h2>
-						</header>
-
-					</article>
-
-
-					<article id="post-48-recent" class="post-48 post type-post status-publish format-standard has-post-thumbnail hentry category-fashion category-photo category-uncategorized tag-design">
-
-						<div class="featured-image">
-							<a href="https://wpstash.com/fashify/2016/07/06/5-things-to-consider-before-buying-an-apple-watch/" title="5 Things to Consider Before Buying an Apple Watch"><img width="280" height="220" src="https://wpstash.com/fashify/wp-content/uploads/sites/5/2016/07/pexels-photo-desk-280x220.jpg" class="attachment-fashify-thumb-layout3 size-fashify-thumb-layout3 wp-post-image" alt="" /></a>				</div>
-
-							<header class="entry-header">
-
-								<h2 class="entry-title"><a href="https://wpstash.com/fashify/2016/07/06/5-things-to-consider-before-buying-an-apple-watch/" rel="bookmark">5 Things to Consider Before Buying an Apple Watch</a></h2>
-							</header>
-
-						</article>
-
-
-						<article id="post-46-recent" class="post-46 post type-post status-publish format-standard has-post-thumbnail hentry category-photo category-uncategorized">
-
-							<div class="featured-image">
-								<a href="https://wpstash.com/fashify/2016/07/06/nor-again-is-there-anyone-who-loves-or-pursues-or-desires/" title="Nor again is there anyone who loves or pursues or desires"><img width="280" height="220" src="https://wpstash.com/fashify/wp-content/uploads/sites/5/2016/07/person-woman-relaxation-girl-280x220.jpg" class="attachment-fashify-thumb-layout3 size-fashify-thumb-layout3 wp-post-image" alt="" /></a>				</div>
-
-								<header class="entry-header">
-
-									<h2 class="entry-title"><a href="https://wpstash.com/fashify/2016/07/06/nor-again-is-there-anyone-who-loves-or-pursues-or-desires/" rel="bookmark">Nor again is there anyone who loves or pursues or desires</a></h2>
-								</header>
-
-							</article>
+				<div class="site-copyright">
+					<?php printf( esc_html__( 'Copyright &copy; %1$s %2$s. All Rights Reserved.', 'fashify' ), date_i18n( __('Y', 'fashify') ), get_bloginfo( 'name' ) ); ?>
+				</div>
 
 
-							<article id="post-42-recent" class="post-42 post type-post status-publish format-standard has-post-thumbnail hentry category-photo category-uncategorized category-video">
-
-								<div class="featured-image">
-									<a href="https://wpstash.com/fashify/2016/07/06/here-are-10-youth-innovators-from-ages-seven-to-15/" title="Here are 10 youth innovators, from ages seven to 15"><img width="280" height="220" src="https://wpstash.com/fashify/wp-content/uploads/sites/5/2016/07/coffee-apple-iphone-laptop-280x220.jpg" class="attachment-fashify-thumb-layout3 size-fashify-thumb-layout3 wp-post-image" alt="" /></a>				</div>
-
-									<header class="entry-header">
-
-										<h2 class="entry-title"><a href="https://wpstash.com/fashify/2016/07/06/here-are-10-youth-innovators-from-ages-seven-to-15/" rel="bookmark">Here are 10 youth innovators, from ages seven to 15</a></h2>
-									</header>
-
-								</article>
+				<?php do_action('fashify_theme_info'); ?>
 
 
-							</div>
-						</div>
+			</div>
+		</div><!-- .site-info -->
 
-					</div>
+	</footer><!-- #colophon -->
+</div><!-- #page -->
 
-					<div class="footer-widgets">
-						<div class="container">
-							<div class="footer-inner">
-								<section id="text-2" class="widget widget_text"><h4 class="widget-title">Fashify</h4>			<div class="textwidget"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
-									<address>
-										1600 Amphitheatre Parkway<br />
-										Mountain View <br />
-										CA 94043<br />
-										Call us: <a href="tel:1.800.123.4567">1.800.123.4567</a><br />
-									</address>
-								</div>
-							</section><section id="categories-3" class="widget widget_categories"><h4 class="widget-title">Categories</h4>		<ul>
-								<li class="cat-item cat-item-5"><a href="https://wpstash.com/fashify/category/design/" >Design</a>
-								</li>
-								<li class="cat-item cat-item-6"><a href="https://wpstash.com/fashify/category/fashion/" >Fashion</a>
-								</li>
-								<li class="cat-item cat-item-10"><a href="https://wpstash.com/fashify/category/mobile/" >Mobile</a>
-								</li>
-								<li class="cat-item cat-item-9"><a href="https://wpstash.com/fashify/category/muisic/" >Muisic</a>
-								</li>
-								<li class="cat-item cat-item-7"><a href="https://wpstash.com/fashify/category/photo/" >Photos</a>
-								</li>
-								<li class="cat-item cat-item-1"><a href="https://wpstash.com/fashify/category/uncategorized/" >Uncategorized</a>
-								</li>
-								<li class="cat-item cat-item-8"><a href="https://wpstash.com/fashify/category/video/" >Videos</a>
-								</li>
-							</ul>
-						</section><section id="fashify-posts-2" class="widget fashify-posts-widget"><h4 class="widget-title">Recent Posts</h4>            <ul class="widget-posts">                <li class="has-thumb">
-							<a href="https://wpstash.com/fashify/2016/07/06/samsung-vs-apple-samsung-is-winning-every-way-but-one/" title="Samsung vs. Apple: Samsung Is Winning Every Way But One"><img width="150" height="150" src="https://wpstash.com/fashify/wp-content/uploads/sites/5/2016/07/pexels-photo-smartphone-macbook-150x150.jpg" class="attachment-thumbnail size-thumbnail wp-post-image" alt="" /></a>                    <div class="p-info">
-								<h2 class="entry-title"><a title="Samsung vs. Apple: Samsung Is Winning Every Way But One" href="https://wpstash.com/fashify/2016/07/06/samsung-vs-apple-samsung-is-winning-every-way-but-one/" rel="bookmark">Samsung vs. Apple: Samsung Is Winning Every Way But One</a></h2>
-								<span class="entry-date">July 6, 2016</span>                    </div>
-							</li>
-							<li class="has-thumb">
-								<a href="https://wpstash.com/fashify/2016/07/06/adeles-album-will-not-be-on-streaming-sites/" title="Adele’s Album Will Not Be On Streaming Sites"><img width="150" height="150" src="https://wpstash.com/fashify/wp-content/uploads/sites/5/2016/07/coffee-apple-iphone-laptop-2-150x150.jpg" class="attachment-thumbnail size-thumbnail wp-post-image" alt="" /></a>                    <div class="p-info">
-									<h2 class="entry-title"><a title="Adele’s Album Will Not Be On Streaming Sites" href="https://wpstash.com/fashify/2016/07/06/adeles-album-will-not-be-on-streaming-sites/" rel="bookmark">Adele’s Album Will Not Be On Streaming Sites</a></h2>
-									<span class="entry-date">July 6, 2016</span>                    </div>
-								</li>
-								<li class="has-thumb">
-									<a href="https://wpstash.com/fashify/2016/07/06/5-things-to-consider-before-buying-an-apple-watch/" title="5 Things to Consider Before Buying an Apple Watch"><img width="150" height="150" src="https://wpstash.com/fashify/wp-content/uploads/sites/5/2016/07/pexels-photo-desk-150x150.jpg" class="attachment-thumbnail size-thumbnail wp-post-image" alt="" /></a>                    <div class="p-info">
-										<h2 class="entry-title"><a title="5 Things to Consider Before Buying an Apple Watch" href="https://wpstash.com/fashify/2016/07/06/5-things-to-consider-before-buying-an-apple-watch/" rel="bookmark">5 Things to Consider Before Buying an Apple Watch</a></h2>
-										<span class="entry-date">July 6, 2016</span>                    </div>
-									</li>
-								</ul></section>				</div>
-							</div>
-						</div>
-
-						<div class="site-info">
-							<div class="container">
-
-								<div class="site-copyright">
-									Copyright &copy; 2018 Fashify. All Rights Reserved.				</div>
-
-
-									<div class="theme-info-text">
-										Fashify Theme by <a href="https://www.famethemes.com/" rel="designer">FameThemes</a>		</div>
-
-
-									</div>
-								</div><!-- .site-info -->
-
-							</footer><!-- #colophon -->
-						</div><!-- #page -->
-
-						<script type='text/javascript'>
-						/* <![CDATA[ */
-						var wpcf7 = {"apiSettings":{"root":"https:\/\/wpstash.com\/fashify\/wp-json\/contact-form-7\/v1","namespace":"contact-form-7\/v1"},"recaptcha":{"messages":{"empty":"Please verify that you are not a robot."}},"cached":"1"};
-						/* ]]> */
-						</script>
-						<script type='text/javascript' src='https://wpstash.com/fashify/wp-content/plugins/contact-form-7/includes/js/scripts.js?ver=4.9'></script>
-						<script type='text/javascript' src='https://wpstash.com/fashify/wp-content/themes/fashify/assets/js/navigation.js?ver=20151215'></script>
-						<script type='text/javascript' src='https://wpstash.com/fashify/wp-content/themes/fashify/assets/js/skip-link-focus-fix.js?ver=20151215'></script>
-						<script type='text/javascript' src='https://wpstash.com/fashify/wp-includes/js/wp-embed.min.js?ver=4.9.8'></script>
+<?php wp_footer(); ?>
 
 </body>
 </html>
--- bitrix/templates/fashify/header.php
+++ bitrix/templates/fashify/header.php
@@ -1,138 +1,81 @@
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
-<!DOCTYPE html>
-<html lang="en-US">
+<?php
+/**
+ * The header for our theme.
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Fashify
+ */
+
+?><!DOCTYPE html>
+<html>
 <head>
+<meta http-equiv="Content-Type" content="text/html;charset=<?=SITE_CHARSET?>"/>
+<meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="<?=SITE_TEMPLATE_PATH?>/style.css" />
 
-	<title><?$APPLICATION->ShowTitle()?></title>
+<title><?$APPLICATION->ShowTitle()?></title>
+<?$APPLICATION->ShowHead();?>
 
-	<link rel="profile" href="http://gmpg.org/xfn/11">
-	<link rel="pingback" href="https://wpstash.com/fashify/xmlrpc.php">
-	<link rel='dns-prefetch' href='//fonts.googleapis.com' />
-	<link rel='dns-prefetch' href='//s.w.org' />
-	<link rel="alternate" type="application/rss+xml" title="Fashify &raquo; Feed" href="https://wpstash.com/fashify/feed/" />
-	<link rel="alternate" type="application/rss+xml" title="Fashify &raquo; Comments Feed" href="https://wpstash.com/fashify/comments/feed/" />
-	<script type="text/javascript">
-	window._wpemojiSettings = {"baseUrl":"https:\/\/s.w.org\/images\/core\/emoji\/11\/72x72\/","ext":".png","svgUrl":"https:\/\/s.w.org\/images\/core\/emoji\/11\/svg\/","svgExt":".svg","source":{"concatemoji":"https:\/\/wpstash.com\/fashify\/wp-includes\/js\/wp-emoji-release.min.js?ver=4.9.8"}};
-	!function(a,b,c){function d(a,b){var c=String.fromCharCode;l.clearRect(0,0,k.width,k.height),l.fillText(c.apply(this,a),0,0);var d=k.toDataURL();l.clearRect(0,0,k.width,k.height),l.fillText(c.apply(this,b),0,0);var e=k.toDataURL();return d===e}function e(a){var b;if(!l||!l.fillText)return!1;switch(l.textBaseline="top",l.font="600 32px Arial",a){case"flag":return!(b=d([55356,56826,55356,56819],[55356,56826,8203,55356,56819]))&&(b=d([55356,57332,56128,56423,56128,56418,56128,56421,56128,56430,56128,56423,56128,56447],[55356,57332,8203,56128,56423,8203,56128,56418,8203,56128,56421,8203,56128,56430,8203,56128,56423,8203,56128,56447]),!b);case"emoji":return b=d([55358,56760,9792,65039],[55358,56760,8203,9792,65039]),!b}return!1}function f(a){var c=b.createElement("script");c.src=a,c.defer=c.type="text/javascript",b.getElementsByTagName("head")[0].appendChild(c)}var g,h,i,j,k=b.createElement("canvas"),l=k.getContext&&k.getContext("2d");for(j=Array("flag","emoji"),c.supports={everything:!0,everythingExceptFlag:!0},i=0;i<j.length;i++)c.supports[j[i]]=e(j[i]),c.supports.everything=c.supports.everything&&c.supports[j[i]],"flag"!==j[i]&&(c.supports.everythingExceptFlag=c.supports.everythingExceptFlag&&c.supports[j[i]]);c.supports.everythingExceptFlag=c.supports.everythingExceptFlag&&!c.supports.flag,c.DOMReady=!1,c.readyCallback=function(){c.DOMReady=!0},c.supports.everything||(h=function(){c.readyCallback()},b.addEventListener?(b.addEventListener("DOMContentLoaded",h,!1),a.addEventListener("load",h,!1)):(a.attachEvent("onload",h),b.attachEvent("onreadystatechange",function(){"complete"===b.readyState&&c.readyCallback()})),g=c.source||{},g.concatemoji?f(g.concatemoji):g.wpemoji&&g.twemoji&&(f(g.twemoji),f(g.wpemoji)))}(window,document,window._wpemojiSettings);
-	</script>
-	<style type="text/css">
-	img.wp-smiley,
-	img.emoji {
-		display: inline !important;
-		border: none !important;
-		box-shadow: none !important;
-		height: 1em !important;
-		width: 1em !important;
-		margin: 0 .07em !important;
-		vertical-align: -0.1em !important;
-		background: none !important;
-		padding: 0 !important;
-	}
-</style>
-<link rel='stylesheet' id='contact-form-7-css'  href='https://wpstash.com/fashify/wp-content/plugins/contact-form-7/includes/css/styles.css?ver=4.9' type='text/css' media='all' />
-<link rel='stylesheet' id='fashify-fonts-css'  href='https://fonts.googleapis.com/css?family=Libre+Franklin%3A300%2C300i%2C400%2C400i%2C600%2C600i%2C800%2C800i&#038;subset=latin%2Clatin-ext' type='text/css' media='all' />
-<link rel='stylesheet' id='font-awesome-css'  href='https://wpstash.com/fashify/wp-content/themes/fashify/assets/css/font-awesome.min.css?ver=4.5' type='text/css' media='all' />
-<link rel='stylesheet' id='fashify-style-css'  href='https://wpstash.com/fashify/wp-content/themes/fashify/style.css?ver=4.9.8' type='text/css' media='all' />
-<style id='fashify-style-inline-css' type='text/css'>
-
-.entry-meta a,
-.main-navigation a:hover,
-.main-navigation .current-menu-item > a,
-.main-navigation .current-menu-ancestor > a,
-.widget_tag_cloud a:hover,
-a:hover,
-.social-links ul a:hover::before
-{
-	color : #f75357;
-}
-button, input[type="button"], input[type="reset"], input[type="submit"]{
-	background: #f75357;
-	border-color : #f75357;
-}
-.widget_tag_cloud a:hover { border-color : #f75357;}
-.main-navigation a,
-h1.entry-title,
-.widget-title,
-.footer-staff-picks h3,
-.navigation .current
-{
-	color: #444;
-}
-button:hover, input[type="button"]:hover,
-input[type="reset"]:hover,
-input[type="submit"]:hover {
-	background: #444;
-	border-color: #444;
-	}.site-header {  background-image: url(https://wpstash.com/fashify/wp-content/uploads/sites/5/2017/01/cropped-coffee-apple-iphone-desk.jpg); background-repeat: no-repeat; background-size: cover; }
-</style>
-<script type='text/javascript' src='https://wpstash.com/fashify/wp-includes/js/jquery/jquery.js?ver=1.12.4'></script>
-<script type='text/javascript' src='https://wpstash.com/fashify/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.4.1'></script>
-<link rel='https://api.w.org/' href='https://wpstash.com/fashify/wp-json/' />
-<link rel="EditURI" type="application/rsd+xml" title="RSD" href="https://wpstash.com/fashify/xmlrpc.php?rsd" />
-<link rel="wlwmanifest" type="application/wlwmanifest+xml" href="https://wpstash.com/fashify/wp-includes/wlwmanifest.xml" />
-<meta name="generator" content="WordPress 4.9.8" />
-<style type="text/css">
-.site-title a,
-.site-description,
-.social-links ul a {
-	color: #ffffff;
-}
-</style>
-<!-- BEGIN GADWP v5.1.1.1 Universal Analytics - https://deconf.com/google-analytics-dashboard-wordpress/ -->
-<script>
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-ga('create', 'UA-91794458-1', 'auto');
-ga('send', 'pageview');
-</script>
-<!-- END GADWP Universal Analytics -->
 </head>
 
-<body class="home blog hfeed homepage-home3">
-	<?$APPLICATION->ShowPanel();?>
-	<div id="page" class="site">
-		<a class="skip-link screen-reader-text" href="#main">Skip to content</a>
+<body>
+<div id="page" class="site">
+	<a class="skip-link screen-reader-text" href="#main">Skip to content</a>
 
-		<header id="masthead" class="site-header" role="banner">
-			<div class="container">
+	<header id="masthead" class="site-header" role="banner">
+		<div class="container">
 
-				<div class="site-branding">
+			<div class="site-branding">
 
+				<?php
+				$site_url="http://".$_SERVER['SERVER_NAME'];
+				$rsSites = CSite::GetByID(SITE_ID);
+				$arSite = $rsSites->Fetch();
+				$siteName = $arSite['SITE_NAME'];
 
-					<h1 class="site-title"><a href="https://wpstash.com/fashify/" rel="home">Fashify</a></h1>
-					<p class="site-description">Just another WPStash Sites site</p>
+				if ( CSite::InDir('/index.php') ) :?>
+					<h1 class="site-title"><?php print_url($site_url.'/index.php',$siteName,'rel="home"') ?></h1>
+				<?php else : ?>
+					<p class="site-title"><?php print_url($site_url.'/index.php',$siteName,'rel="home"') ?></p>
+				<?php
+				endif;
 
-				</div><!-- .site-branding -->
+				$description = "Just another WPStash Sites site"
+				if ( $description ) : ?>
+					<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
+				<?php
+				endif; ?>
 
-				<div class="social-menu">
-					<div id="menu-social" class="social-links"><ul id="menu-social" class="menu"><li id="menu-item-9" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-9"><a href="https://www.facebook.com/"><span class="screen-reader-text">facebook</span></a></li>
+			</div><!-- .site-branding -->
+
+			<div class="social-menu">
+				<div id="menu-social" class="social-links">
+					<ul id="menu-social" class="menu">
+						<li id="menu-item-9" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-9"><a href="https://www.facebook.com/"><span class="screen-reader-text">facebook</span></a></li>
 						<li id="menu-item-10" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-10"><a href="https://twitter.com/"><span class="screen-reader-text">twitter</span></a></li>
 						<li id="menu-item-11" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-11"><a href="https://plus.google.com/u/0/"><span class="screen-reader-text">google plus</span></a></li>
 						<li id="menu-item-12" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-12"><a href="https://www.linkedin.com"><span class="screen-reader-text">linkedin</span></a></li>
 						<li id="menu-item-61" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-61"><a href="https://youtube.com"><span class="screen-reader-text">Youtube</span></a></li>
 						<li id="menu-item-62" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-62"><a href="https://www.pinterest.com/"><span class="screen-reader-text">Pinterest</span></a></li>
-					</ul></div>			</div>
-
+					</ul>
 				</div>
+			</div>
 
-			</header><!-- #masthead -->
+		</div>
 
-			<nav id="site-navigation" class="main-navigation" role="navigation">
-				<div class="container">
+	</header><!-- #masthead -->
 
-					<button class="menu-toggle" aria-controls="top-menu" aria-expanded="false">Menu</button>
-					<div class="menu-main-menu-container"><ul id="primary-menu" class="menu"><li id="menu-item-8" class="menu-item menu-item-type-custom menu-item-object-custom current-menu-item current_page_item menu-item-home menu-item-8"><a href="https://wpstash.com/fashify/">Home</a></li>
-						<li id="menu-item-66" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-66"><a href="https://wpstash.com/fashify/about/">About</a></li>
-						<li id="menu-item-40" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-40"><a href="https://wpstash.com/fashify/category/design/">Design</a></li>
-						<li id="menu-item-41" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-41"><a href="https://wpstash.com/fashify/category/fashion/">Fashion</a></li>
-						<li id="menu-item-44" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-44"><a href="https://wpstash.com/fashify/category/photo/">Photos</a></li>
-						<li id="menu-item-45" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-45"><a href="https://wpstash.com/fashify/category/video/">Videos</a></li>
-						<li id="menu-item-67" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-67"><a href="https://wpstash.com/fashify/contact/">Contact</a></li>
-					</ul></div>
-				</div>
-			</nav><!-- #site-navigation -->
+	<nav id="site-navigation" class="main-navigation" role="navigation">
+		<div class="container">
+
+			<button class="menu-toggle" aria-controls="top-menu" aria-expanded="false">Здесь должно быть меню!</button>
+
+		</div>
+	</nav><!-- #site-navigation -->
+
+	<div id="content" class="site-content">
